c0783006ae3a9afc90bdec0dfce485550166a19e
fadillah <fadillahd544@gmail.com>
2024-10-18 13:46:56 +0700

    Tugas 5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // pastikan ada import ini
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-//Route::get('/profile',[ProfileController::class, 'profile']);
-Route::get('/profile/{nama}',[ProfileController::class, 'profile']);
+Route::get('/', function () {return view('welcome');});
+// Route untuk tugas 2
+Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/user/profile', [ProfileController::class, 'profile']);
+
+// Route untuk tugas 3 & 4
+Route::get('/user/create', function () {return view('create_user');});
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/profile/upload', [ProfileController::class, 'uploadProfilePicture'])->name('upload.profile.picture');
+
+// Route untuk tugas 5
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::resource('users', UserController::class);
