e5d1cb422adef3e6899432702248ba0676a7cde0
fadillah <fadillahd544@gmail.com>
2024-10-24 12:55:29 +0700

    Tugas 7

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // pastikan ada import ini
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-//Route::get('/profile',[ProfileController::class, 'profile']);
-Route::get('/profile/{nama}',[ProfileController::class, 'profile']);
+Route::get('/', function () {return view('welcome');});
+
+Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/user/profile', [ProfileController::class, 'profile']);
+
+
+Route::get('/user/create', function () {return view('create_user');});
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/profile/upload', [ProfileController::class, 'uploadProfilePicture'])->name('upload.profile.picture');
+
+
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::resource('users', UserController::class);
+
+Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
+
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::get('/user', [UserController::class, 'index'])->name('user.list');
